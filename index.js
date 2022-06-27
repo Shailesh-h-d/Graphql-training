dc0daa565382826609d9fb42411f21b539185eaa
Shailesh_H_D <107041533+Shailesh-h-d@users.noreply.github.com>
2022-06-27 21:40:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ const server = new ApolloServer({typeDefs, resolvers, context: ({req, res}) => (
 server.listen().then(({url}) => {
     console.log(`server is up and running at ${url}`);
 });
+
+// edited in github. 
