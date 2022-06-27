e5b24b5183d2554f379e26d4aebb976673e352cb
Shailesh_H_D <shailesh.devaraj@softsuave.com>
2022-06-27 20:50:47 +0530

    added type def definition

--- schema/type-defs.js
+++ schema/type-defs.js
@@ -1,3 +1,4 @@
+// typeDefs is a required argument and should be a GraphQL schema language string or an array of GraphQL schema language strings or a function that takes no arguments and returns an array of GraphQL schema language strings. The order of the strings in the array is not important, but it must include a schema definition.
 const { gql } = require("apollo-server");
 
 const typeDefs = gql`
